1f716120190c9dc3f7eb7e06e51becf347e974a2
lestofante <mombelli.mauro@gmail.com>
2013-05-23 00:44:28 +0200

    still debugging gyro and friends.. uff

--- src/reader/audio/AudioReader.java
+++ src/reader/audio/AudioReader.java
@@ -24,6 +24,8 @@ public class AudioReader extends SensorReader implements Runnable{
 	private Vector3f testVec;
 	boolean stampaValori = false;
 	public final AtomicBoolean leggi = new AtomicBoolean(true);
+	
+	final static boolean debugByte = false;
 
 	int lettureValide=0;
 	
@@ -77,10 +79,14 @@ public class AudioReader extends SensorReader implements Runnable{
 		long tempo = System.currentTimeMillis();
 		long deltaT, min=Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 		while( leggi.get() ){
+			System.out.println("Leggo dati");
 			read = microphone.read(buffer, 0, buffer.length ); //we must read 2 byte at time
+			System.out.println("letti");
 			sum+=read;
-			
+			//readok=0;
+			System.out.println("analizzo");
 			analyze(buffer);
+			System.out.println("analizzati");
 			
 			if (min > read){
 				min = read;
@@ -116,6 +122,26 @@ public class AudioReader extends SensorReader implements Runnable{
 		
 		System.out.println("Lettura scheda audio terminata su richiesta");
 	}
+	/*
+	private static final byte BitReverseTable256[] = 
+		{
+		  0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
+		  0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
+		  0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
+		  0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
+		  0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 
+		  0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+		  0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 
+		  0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+		  0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+		  0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 
+		  0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+		  0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+		  0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 
+		  0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+		  0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 
+		  0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+		};*/
 	
 	private synchronized void analyze(byte[] read) {
 		
@@ -126,6 +152,9 @@ public class AudioReader extends SensorReader implements Runnable{
 			boolean validByte = true;
 			int i=findOccurence;
 			char sensore = (char) read[i];
+			
+			if (debugByte)
+				System.out.println(sensore);
 			
 			if (sensore == 'b'){
 				//fine del pacchetto!!
@@ -151,31 +180,83 @@ public class AudioReader extends SensorReader implements Runnable{
 						if ( read[i]==occorrenze[o] )
 							validByte = false; //don't take care of next byte
 					}
-					int val;
+					short val;
 					switch(byteLettiValidi){
 					case 0:
-						ls = read[i] & 0xff;
+						if (debugByte)
+							System.out.println("x");
+						
+						ls=0;
+						ls |= read[i]&0xff;
+						
+						if (debugByte)
+							System.out.println("ls: "+toBinary(ls) );
+						
 						break;
 					case 1:
-						ms = read[i];
-						val=(ms<<8) | ls;
+						ms=0;
+						ms = read[i]&0xff;
+						
+						if (debugByte)
+							System.out.println("ms: "+toBinary(ms) );
+
+						val = 0;
+						val = concatenate1(ms, ls);
+						concatenate2(ms, ls);
+						
 						lettura.x = val;
+						
+
 						break;
 					case 2:
+						if (debugByte)
+							System.out.println("y");
+						
+						ls=0;
 						ls = read[i]& 0xff;
+						
+						if (debugByte)
+							System.out.println("ls: "+toBinary(ls) );
+						
 						break;
 					case 3:
-						ms = read[i];
-						val=(ms<<8) | ls;
+						ms=0;
+						ms = read[i]&0xff;
+						
+						if (debugByte)
+							System.out.println("ms: "+toBinary(ms) );
+						
+						val = 0;
+						val = concatenate1(ms, ls);
+						concatenate2(ms, ls);
+						
 						lettura.y = val;
+						
 						break;
 					case 4:
+						if (debugByte)
+							System.out.println("z");
+						
+						ls=0;
 						ls = read[i]& 0xff;
+						
+						if (debugByte)
+							System.out.println("ls: "+toBinary(ls) );
+						
 						break;
 					case 5:
-						ms = read[i];
-						val= (ms<<8) | ls;
+						ms=0;
+						ms = read[i]&0xff;
+						
+						if (debugByte)
+							System.out.println("ms: "+toBinary(ms) );
+						
+						val = 0;
+						val = concatenate1(ms, ls);
+						concatenate2(ms, ls);
+						
 						lettura.z = val;
+						
 						//System.out.println("letto: "+lettura);
 						break;
 					default:
@@ -212,6 +293,7 @@ public class AudioReader extends SensorReader implements Runnable{
 					//System.out.println("test vect: "+lettura);
 					break;
 				case 'S':
+					testVec = lettura;
 					diffMs++;
 					//System.out.println("test vect: "+lettura);
 					break;
@@ -219,7 +301,7 @@ public class AudioReader extends SensorReader implements Runnable{
 					System.out.println("grave errore sensore "+sensore);
 				}
 			}
-			
+			/*
 			if (gyroVec != null && accVec != null && magVec != null) {
 				if (stampaValori){
 					System.out.println("Gyro:"+gyroVec+" acc: "+accVec+" magne: "+magVec+" test: "+testVec);
@@ -235,6 +317,7 @@ public class AudioReader extends SensorReader implements Runnable{
 				gyroVec = accVec = magVec = null;
 				
 			}
+			*/
 			/*
 			if (gyroVec != null && accVec != null) {
 				if (stampaValori){
@@ -247,14 +330,114 @@ public class AudioReader extends SensorReader implements Runnable{
 							accVec.x, accVec.y, accVec.z, 
 							0, 0, 0);
 				}
+				calibra(gyroVec, accVec, new Vector3f());
 				gyroVec = accVec = null;
 			}
 			*/
+			if (gyroVec != null ) {
+				if (stampaValori){
+					System.out.println("Gyro:"+gyroVec);
+					stampaValori = false;
+				}
+				
+				if (dcm != null) {
+					dcm.MadgwickAHRSupdate(gyroVec.x, gyroVec.y, gyroVec.z, 
+							0, 0, 1, 
+							0, 0, 0);
+				}
+				calibra(gyroVec, new Vector3f(), new Vector3f());
+				gyroVec = null;
+			}
+			
 		}
 	}
 	
+	private short concatenate2(int ms, int ls) {
+		short val=0;
+		val|=(ls<<8) | ms;
+		if (debugByte){
+			System.out.println("val3: "+val);
+			if (Math.abs(val) > SOGLIA_GYRO)
+				System.out.println("sfaso3");
+		}
+		val = reverse(val);
+		if (debugByte){
+			System.out.println("val4: "+val);
+			if (Math.abs(val) > SOGLIA_GYRO)
+				System.out.println("sfaso4");
+		}
+		return val;
+	}
+
+	int readok=0;
+	private short concatenate1(int ms, int ls) {
+		short val=0;
+		val|=(ms<<8) | ls;
+		if (debugByte){
+			System.out.println("val1: "+val);
+			if (Math.abs(val) > SOGLIA_GYRO)
+				System.out.println("sfaso1");
+		}
+		val = reverse(val);
+		if (debugByte){
+			System.out.println("val2: "+val);
+			if (Math.abs(val) > SOGLIA_GYRO)
+				System.out.println("sfaso2");
+		}
+		
+		if (Math.abs(val) > SOGLIA_GYRO){
+			System.out.println("ko "+val+" read ok was "+readok);
+			readok=0;
+		}else{
+			System.out.println("ok "+val);
+			readok++;
+		}
+		return val;
+	}
+
+	private String toBinary(int b) {
+		for (int i=0; i< bitReverse; i++){
+			System.out.print( (b&(1<<i))!=0?'1':'0');
+		}
+		System.out.print(" ");
+		return b+"";
+	}
+	
+	private static final int bitReverse = 16;
+	private short reverse(short x) {
+		short b=0;
+		boolean temp;
+		for (int i=0; i< bitReverse; i++){
+			temp = (x&(1<<i))!=0?true:false;
+			if (temp){
+				b |= 1<<(bitReverse-1-i);
+			}
+		}
+		/*
+		while ( (x & 0xff) != 0){
+			b<<=1;
+			b|=( x &1);
+			x>>=1;
+		}
+		*/
+		if (debugByte){
+			System.out.print("was: ");
+			for (int i=0; i< bitReverse; i++){
+				System.out.print( (x&(1<<i))!=0?'1':'0');
+			}
+			System.out.println();
+		
+			System.out.print("is: ");
+			for (int i=0; i< bitReverse; i++){
+				System.out.print( (b&(1<<i))!=0?'1':'0');
+			}
+			System.out.println();
+		}
+		return b;
+	}
+	
 	Vector3f lastGyro = null, lastAcc=null, lastMag = null;
-	private float SOGLIA_GYRO = 1000;
+	private float SOGLIA_GYRO = 2000;
 	private int diffGyro = 0, diffAcc = 0, diffMagne = 0, diffTest=0, diffMs=0;
 	private void calibra(Vector3f gyroVec, Vector3f accVec, Vector3f magVec) {
 		if (lastGyro==null || lastAcc==null || lastMag==null){
@@ -295,8 +478,20 @@ public class AudioReader extends SensorReader implements Runnable{
 	
 	private void testSoglia(float a, float b, float soglia, String nome){
 		if ( Math.abs(a - b) > soglia  ){
-			boolean zero = false;
+			//boolean zero = false;
 			System.out.println( nome+"\tdiff: "+(a - b)+"\ta:"+a+"\tb:"+b );
+			int x = (int) a, y = (int) b;
+			System.out.print("was: ");
+			for (int i=0; i< 16; i++){
+				System.out.print( (x&(1<<i))!=0?'1':'0');
+			}
+			System.out.println();
+			
+			System.out.print("is: ");
+			for (int i=0; i< 16; i++){
+				System.out.print( (y&(1<<i))!=0?'1':'0');
+			}
+			System.out.println();
 		}
 	}
 
