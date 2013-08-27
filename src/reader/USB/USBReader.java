bf5da9f1db2c6fb8dbe358b8feaa4e93bfa6f34a
lestofante <mombelli.mauro@gmail.com>
2013-08-27 23:42:37 +0200

    added magnetometer

--- src/reader/USB/USBReader.java
+++ src/reader/USB/USBReader.java
@@ -23,6 +23,9 @@ public class USBReader extends SensorReader implements USBLIstener{
 	
 	short ax, ay, az;
 	boolean accOk = false;
+	private short mz;
+	private short my;
+	private short mx;
 	static float toRad = 35f/32768f;
 	
 	@Override
@@ -35,16 +38,20 @@ public class USBReader extends SensorReader implements USBLIstener{
 
 	@Override
 	public void setRawMagnetometer(short x, short y, short z) {
-		// TODO Auto-generated method stub
-		
+		this.mx = x;
+		this.my = y;
+		this.mz = z;
 	}
 
 	@Override
 	public void setRawGyroscope(short x, short y, short z) {
 		if (accOk==true){
 			//System.out.println("Valori giro"+x+" "+y+ " "+z);
-			dcm.FreeIMUUpdate(-x*toRad, -y*toRad, z*toRad, -this.ay, this.ax, this.az, 0, 0, 0);
+			//my = mx = mz = 0;
+			dcm.FreeIMUUpdate(-x*toRad, -y*toRad, z*toRad, -this.ay, this.ax, this.az, -this.mz, this.mx, this.my);
 			accOk = false;
+			my = mx = mz = 0;
+			ay = ax = az = 0;
 		}
 		
 	}
